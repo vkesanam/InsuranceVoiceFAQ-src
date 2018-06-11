37fc5b78c6ab7a1fc0b1f8e8a473146867c552e9
Venkata Sambasiva Rao Kesanam <Venkata.Rao@isdbwdom.local>
2018-06-11 11:57:38 +0400

    Add project files.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=insurancevoicefaq-Web-Deploy.pubxml -p:Password=zm0qbl7aG54isizv7rbkCB4FlWP4wd3rxSPu6eSxbCtfGEhLWk2yYiAfXCrX
+
